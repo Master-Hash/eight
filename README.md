c694ef841deb044b3cdeec2ed36995e67a285620
Master-Hash <56669192+Master-Hash@users.noreply.github.com>
2021-06-08 02:18:36 +0800

    判断出胜者 OK

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # eight
 凑八游戏 - Powered by React
+
+~~原谅我把教程的井字棋写完再写凑八（~~
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props: any) {
+function Square(props: {value: null | "x" | "o", onClick: any}) {
   return (
-    <button className="square">
-      {}
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value ? props.value.toUpperCase() : ""}
     </button>
   );
 }
 
 function Board(props: any) {
-  function renderSquare(i: number) {
-    return <Square />;
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [round, setRound] = useState(0);
+  let status = `Next player: ${round & 1 ? "O" : "X"}`;
+  const winner = calculateWinner(squares);
+  if (winner) {
+    status = 'Winner: ' + winner;
   }
-  const status = "Next player: X";
+
+  function handleClick(i: number) {
+    if (winner) { return; }
+    const newSquares = squares.slice();
+    newSquares[i] = round & 1 ? "o" : "x";
+    setSquares(newSquares);
+    setRound(round + 1);
+  }
+
+  function renderSquare(i: number) {
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => handleClick(i)}
+      />
+    );
+  }
+
   return (
     <div>
       <div className="status">{status}</div>
@@ -51,9 +75,29 @@ function Game(props: any) {
   );
 }
 
+function calculateWinner(squares: Array<null | "x" | "o">) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 // ========================================
 
 ReactDOM.render(
   <Game />,
-  document.getElementById('root')
+  document.querySelector('#root')
 );
